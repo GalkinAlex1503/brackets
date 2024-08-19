7edb895fdf7506297623b26b5cee4b2a8c1c6af0
GalkinAlex1503 <galkin.alexandr1503@gmail.com>
2024-08-19 19:38:04 +0200

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let stack = [];
+    let bracketMap = new Map();
+    let sameBracketPairs = new Set();
+
+    for (let [open, close] of bracketsConfig) {
+        bracketMap.set(close, open);
+        if (open === close) {
+            sameBracketPairs.add(open);
+        }
+    }
+
+    for (let char of str) {
+        if (sameBracketPairs.has(char)) {
+            if (stack.length > 0 && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (bracketMap.has(char)) {
+            if (stack.length === 0 || stack[stack.length - 1] !== bracketMap.get(char)) {
+                return false;
+            }
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 }
